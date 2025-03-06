5c0b274174c87c57e8443a01da5358bafd3f211b
vladiwanov <3298901@gmail.com>
2025-03-07 01:57:47 +0200

    added phone number to contacts in footer

--- src/content-components/Contacts/Contacts.js
+++ src/content-components/Contacts/Contacts.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import LanguageContentSelector from 'additional-components/LanguageContentSelector';
 import { getLanguageMemoised } from 'redux/languages/languages-selector';
 import io from 'tools/io';
+import sprite from '../../utils/db/img/sprite.svg';
 
 export default function Contacts() {
   const currentLanguage = useSelector(getLanguageMemoised);
@@ -17,7 +18,8 @@ export default function Contacts() {
   return (
     <section className={s.footerItems}>
       <p>{footer.contacts}</p>
-      <div>
+      {/*<div className={` list ${s.contacts}`}>*/}
+        <div >
         <a
           className={s.contacts}
           href="mailto: iv@iv-en.com"
@@ -27,6 +29,19 @@ export default function Contacts() {
         >
           <MdMail className={s.footerIcons} />
           <p className={s.link}>iv@iv-en.com</p>
+        </a>
+        <a
+          className={s.contacts}
+          href="tel:380 66 383 27 47"
+          target="_blank"
+          rel="noreferrer"
+          id="contacts"
+        >
+          <svg className={s.footerIcons}>
+            <use href={sprite + '#phone'} />
+          </svg>
+          {/*<MdMail className={s.footerIcons} />*/}
+          <p className={s.link}>380 66 383 27 47</p>
         </a>
       </div>
     </section>
